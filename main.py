23e64c3037872a4bdff6f6adfe75c19d6ed6ec19
sjang42 <jexjang92@gmail.com>
2018-07-28 10:16:52 +0900

    Add process train

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bigram_counter_vector import BigramCounterVector
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from tools import csv2dictlist
+import torch.nn as nn
 
 
 class NewsDataset(Dataset):
@@ -9,7 +11,7 @@ class NewsDataset(Dataset):
         # TODO
         # 1. shuffle
         # 2. divide as train and test
-        all_dict = csv2dictlist(fname)
+        all_dict = csv2dictlist(fname_parties)
 
         self.data = []
         self.label = []
@@ -21,7 +23,7 @@ class NewsDataset(Dataset):
                 continue
 
             self.data.append(clean_text)
-            self.label.append(-1 if press == '더불어민주당' else 1)
+            self.label.append(0 if press == '더불어민주당' else 1)
 
         self.bigram_cvec = BigramCounterVector(fname_ngram, min_freq=ngram_min_freq)
 
@@ -31,15 +33,74 @@ class NewsDataset(Dataset):
 
         # Conver to Tensor
         data = torch.Tensor(data)
-        label = torch.Tensor([self.label[index]])
+        label = torch.LongTensor([self.label[index]])
 
         return data, label
 
     def __len__(self):
         return len(self.data)
 
+    def get_len_cvec(self):
+        return len(self.bigram_cvec)
+
+
+class LogisticRegression(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(LogisticRegression, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+
+        return out
+
 
 fname = 'data/train/parties_merged.csv'
 ngram_path = 'ngrams/bigram_merged.csv'
+
 news_dataset = NewsDataset(fname_parties=fname, fname_ngram=ngram_path, ngram_min_freq=4)
+
+batch_size = 40
+train_loader = DataLoader(dataset=news_dataset, batch_size=40, shuffle=True)
+
+input_size = news_dataset.get_len_cvec()
+print('input_size: ', input_size)
+
+# set hyper parameter
+hidden_size = 10000
+num_class = 2
+
+learning_rate = 0.0001
+num_epoch = 10
+
+model = LogisticRegression(input_size, hidden_size, num_class)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# train model
+total_step = len(train_loader)
+for epoch in range(num_epoch):
+    print('epoch start')
+    for i, (data, label) in enumerate(train_loader):
+        label = label.squeeze()
+        outputs = model(data)
+        loss = criterion(outputs, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if (i + 1) % 1 == 0:
+            print('Epoch : [{}/{}], Step : [{}/{}], Loss : {:.4f}'.format(
+                epoch + 1, num_epoch, i + 1, total_step, loss.item()
+            ))
+
+    if (epoch + 1) % 1 == 0:
+        print('Epoch : [{}/{}], Loss : {:.4f}'.format(
+            epoch + 1, num_epoch, loss.item()
+        ))
+
+# test model
 
